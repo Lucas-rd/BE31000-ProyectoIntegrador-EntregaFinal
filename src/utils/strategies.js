532644757cd679152f487d9794b84be7b75ae7a7
everisit <everisit@BAI-6YB6DB3.usersad.everis.int>
2022-11-15 21:19:14 -0300

    git gud

--- src/utils/strategies.js
+++ src/utils/strategies.js
@@ -22,7 +22,7 @@ const registerStrategy = new LocalStrategy(
                 lastName: req.body.lastName,
                 email: req.body.email,
                 phoneNumber: req.body.phoneNumber ,
-                avatar: `http://localhost:${enviroment.PORT}/${req.file.filename}`,
+                avatar: `https://proy-int-mtg-ecommerce.herokuapp.com/${req.file.filename}`,
                 role: 'user'
             }
             const createdUser = await User.create(newUser)
